8f4b8fccd141e84945d72c0461eb7d8566fc8699
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2021-04-28 01:57:51 +0300

    added material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './core/componets/home/home.component';
 import {PageNotFoundComponent} from './core/componets/page-not-found/page-not-found.component';
+import {LoginComponent} from './core/componets/login/login.component';
 
 const routes: Routes = [
   // {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: '**', component: PageNotFoundComponent}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {SharedModule} from './shared/shared.module';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import {CoreModule} from './core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import {CoreModule} from './core/core.module';
     SharedModule,
     CoreModule,
     GraphQLModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
